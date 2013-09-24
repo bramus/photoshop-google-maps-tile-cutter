4900c536e1fc121593637ccef2a6549064678aba
Bramus <bramus@bram.us>
2013-09-24 21:29:26 +0200

    Add a UI Dialog (fixes #2, thanks @nspringer for kickstarting this)

--- PS_Bramus.GoogleMapsTileCutter.jsx
+++ PS_Bramus.GoogleMapsTileCutter.jsx
@@ -3,34 +3,13 @@
  * By Bramus Van Damme - http://www.bram.us/
  *
  * Based upon work by Will James (http://onNYTurf.com), Curtis Wyatt (http://gocalipso.com/), and Nate Bundy (http://www.lemonrage.com/)
+ * UI Additions intially provided by Nick Springer (http://www.springercartographics.com/)
  *
  */
 
+// **** HELPER FUNCTIONS
 
-
-// **** CONFIG - FEEL FREE TO ADJUST
-
-// path where to save the tiles
-var targetPath = Folder.desktop + "/tilecutter/"; // be sure to include the trailing slash!
-
-// size of the tiles - default Google Maps size: 256
-var TILE_SIZE = 256;
-
-// should we save empty (viz. totally transparent) tiles? - default: false
-var saveTransparentTiles = false;
-
-// In which format(s) should be save the tiles? - default: jpg only
-var saveJPEG = true;
-var savePNG = false;
-var saveGIF = false;
-
-// wanted backgroundcolor to fill in the blanks when resizing the image
-var bgColor = '000000';
-
-
-// **** HELPER FUNCTIONS - DON'T TOUCH!
-
-// Takes a snapshot
+// Take a snapshot
 function takeSnapshot() {
 	var desc153 = new ActionDescriptor();
 	var ref119 = new ActionReference();
@@ -42,7 +21,7 @@ function takeSnapshot() {
 	executeAction(charIDToTypeID("Mk  "), desc153, DialogModes.NO );
 }
 
-// deletes the current active snapshot
+// Delete the current active snapshot
 function deleteCurrentSnapshot() {
 	var ref = new ActionReference();
 	ref.putProperty(charIDToTypeID("HstS"), charIDToTypeID("CrnH"));
@@ -51,12 +30,12 @@ function deleteCurrentSnapshot() {
 	executeAction(charIDToTypeID("Dlt "), desc, DialogModes.NO );
 }
 
-// reversts to a given snapshot
+// Revert to a given snapshot
 function revertToSnapshot(doc, snapshotID) {
 	doc.activeHistoryState = doc.historyStates[snapshotID];
 }
 
-// get id of the last snapshot
+// Get id of the last snapshot
 function getLastSnapshotID(doc) {
 	var hsObj = doc.historyStates;
 	var hsLength = hsObj.length;
@@ -67,7 +46,7 @@ function getLastSnapshotID(doc) {
 	}
 }
 
-// check if a layer is empty
+// Check if a layer is empty
 function isLayerEmpty(doc, layer) {
 	if (!doc) {
 		doc = app.activeDocument;
@@ -78,22 +57,15 @@ function isLayerEmpty(doc, layer) {
 	return parseInt(layer.bounds.toString().replace(/\D/g,""), 10) === 0;
 }
 
-
-
-// **** MAIN CORE - DON'T TOUCH!
-
-// No active document, stop here
-if (app.documents.length === 0) {
-	alert("Please open a file", "TileCutter Error", true);
-	app.beep();
-}
-
-// Active document and folder exists, process it
-else {
+// Cut the active document into tiles using the passed in settings
+function cutTiles(options, tickCallback) {
 
 	// Make sure targetPath exists
-	var targetFolder = new Folder(targetPath);
+	var targetFolder = new Folder(options.targetPath);
 	if (!targetFolder.exists) targetFolder.create();
+
+	// Add trailing / to targetPath
+	options.targetPath += ((File.fs == 'Windows') ? '\'' : '/');
 
 	// Make sure we're using pixels
 	var startRulerUnits = app.preferences.rulerUnits;
@@ -103,13 +75,15 @@ else {
 	var curDoc = app.activeDocument;
 
 	// Define max tile dimensions
-	var maxTileDim = Math.ceil(Math.max(curDoc.width.value, curDoc.height.value) / TILE_SIZE);
+	var maxTileDim = Math.ceil(Math.max(curDoc.width.value, curDoc.height.value) / options.tileSize);
 
-	// Define max and min zoom level
+	// Define max and min zoom level + define the total number of tiles that will be generated
 	var minZoomLevel = 0,
-		maxZoomLevel = 0;
+		maxZoomLevel = 0,
+		numTilesTotalForAllLevels = 1;
 	do {
 		maxZoomLevel++;
+		numTilesTotalForAllLevels += Math.pow(2, 2 * maxZoomLevel);
 	} while (Math.pow(2, maxZoomLevel) < maxTileDim);
 
 	// Store initial state
@@ -127,18 +101,20 @@ else {
 
 	// Resize to a square version & center image - this way we get an image size that's easily tileable (512, 1028, 2048, 4096, ...)
 	var bgColorHex = new SolidColor();
-	bgColorHex.rgb.hexValue = bgColor;
+	bgColorHex.rgb.hexValue = options.bgColor;
 	app.backgroundColor = bgColorHex;
-	curDoc.resizeCanvas(TILE_SIZE * Math.pow(2, maxZoomLevel), TILE_SIZE * Math.pow(2, maxZoomLevel), AnchorPosition.MIDDLECENTER);
+	curDoc.resizeCanvas(options.tileSize * Math.pow(2, maxZoomLevel), options.tileSize * Math.pow(2, maxZoomLevel), AnchorPosition.MIDDLECENTER);
 
 	// Store current zoom level
-	var ZoomLevel = maxZoomLevel;
+	var zoomLevel = maxZoomLevel;
+
+	var curTileNum = 0;
 
 	// Do the following for each zoom level the user wants
-	while (ZoomLevel >= minZoomLevel) {
+	while (zoomLevel >= minZoomLevel) {
 
 		// Resize the canvas to fit the zoom level (50% per zoom level step)
-		if (ZoomLevel < maxZoomLevel) {
+		if (zoomLevel < maxZoomLevel) {
 			curDoc.resizeImage(curDoc.width.value * 0.5, curDoc.height.value * 0.5);
 		}
 
@@ -147,8 +123,8 @@ else {
 		snapshots.push(getLastSnapshotID(curDoc));
 
 		// Calculate the number of tiles we'll need
-		var numTilesX = parseInt(curDoc.width.value, 10) / TILE_SIZE; // num tiles on the x axis
-		var numTilesY = parseInt(curDoc.height.value, 10) / TILE_SIZE; // num tiles on the y axis
+		var numTilesX = parseInt(curDoc.width.value, 10) / options.tileSize; // num tiles on the x axis
+		var numTilesY = parseInt(curDoc.height.value, 10) / options.tileSize; // num tiles on the y axis
 		var numTilesTotal = numTilesX * numTilesY; // total tiles (numTilesX * numTilesY)
 
 		// Counters to track which x value and which y value we are on in our image tile grid
@@ -159,31 +135,40 @@ else {
 		// For each tile we need to make, we repeat each step in this loop
 		for (n = 1; n < numTilesTotal + 1; n++) {
 
+			// Increment curTileNum
+			curTileNum++;
+
 			// We cut up tiles column by column
 			// I.E. we cut up all the tiles for a given x value before moving on to the next x value.
 			// We do this by checking if the y value we are on is the last tile in a column
 			// We compare our y counter to our total y number of Tiles, if they are the same is we do the following
 			if (parseInt(curTileY, 10) == parseInt(numTilesY, 10)) {
-
 				curTileX += 1; // move to next column
 				curTileY = 0; // start back at the top
 			}
 
 			// Crop out needed square tile
-			curDoc.crop(Array(curTileX * TILE_SIZE, curTileY * TILE_SIZE, curTileX * TILE_SIZE + TILE_SIZE, curTileY * TILE_SIZE + TILE_SIZE));
+			curDoc.crop([
+				curTileX * options.tileSize,
+				curTileY * options.tileSize,
+				(curTileX * options.tileSize) + options.tileSize,
+				(curTileY * options.tileSize) + options.tileSize
+			]);
 
-			if (saveTransparentTiles || !isLayerEmpty(curDoc, curDoc.activeLayer)) {
+			// alert('Cropped from ' + (curTileX * options.tileSize) + ',' + (curTileY * options.tileSize) + ' to ' + ((curTileX * options.tileSize) + options.tileSize) + ',' + ((curTileY * options.tileSize) + options.tileSize) + '.');
+
+			if (options.saveTransparentTiles || !isLayerEmpty(curDoc, curDoc.activeLayer)) {
 
 				// Set the active layer to a background layer so that our bgColor is used as background color
 				curDoc.activeLayer.isBackgroundLayer = true;
 
 				//Save the file
-				if (saveGIF) {
+				if (options.saveGIF) {
 
 					//Set path to file and file name
-					saveFile = new File(targetPath + ZoomLevel + "_" + curTileX + "_" + curTileY + ".gif");
+					var saveFile = new File(options.targetPath + zoomLevel + "_" + curTileX + "_" + curTileY + ".gif");
 					//Set save options
-					gifSaveOptions = new GIFSaveOptions();
+					var gifSaveOptions = new GIFSaveOptions();
 					gifSaveOptions.colors = 64;
 					gifSaveOptions.dither = Dither.NONE;
 					gifSaveOptions.matte = MatteType.NONE;
@@ -191,26 +176,29 @@ else {
 					gifSaveOptions.transparency = 1;
 					gifSaveOptions.interlaced = 0;
 					curDoc.saveAs(saveFile, gifSaveOptions, true, Extension.LOWERCASE);
+
 				}
 
-				if (savePNG) {
+				if (options.savePNG) {
 
 					//Set path to file and file name
-					saveFile = new File(targetPath + ZoomLevel + "_" + curTileX + "_" + curTileY + ".png");
-					pngSaveOptions = new PNGSaveOptions();
+					var saveFile = new File(options.targetPath + zoomLevel + "_" + curTileX + "_" + curTileY + ".png");
+					var pngSaveOptions = new PNGSaveOptions();
 					pngSaveOptions.interlaced = 0;
 					curDoc.saveAs(saveFile, pngSaveOptions, true, Extension.LOWERCASE);
+
 				}
 
-				if (saveJPEG) {
+				if (options.saveJPEG) {
 
 					//Set path to file and file name
-					saveFile = new File(targetPath + ZoomLevel + "_" + curTileX + "_" + curTileY + ".jpg");
-					jpegSaveOptions = new JPEGSaveOptions();
+					var saveFile = new File(options.targetPath + zoomLevel + "_" + curTileX + "_" + curTileY + ".jpg");
+					var jpegSaveOptions = new JPEGSaveOptions();
 					jpegSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
 					jpegSaveOptions.matte = MatteType.NONE;
 					jpegSaveOptions.quality = 5;
 					curDoc.saveAs(saveFile, jpegSaveOptions, true, Extension.LOWERCASE);
+
 				}
 
 			}
@@ -221,10 +209,13 @@ else {
 			// Move to next tile in column
 			curTileY += 1;
 
+			// call tickCallback
+			tickCallback(curTileNum, numTilesTotalForAllLevels);
+
 		}
 
 		// move to next zoom level
-		ZoomLevel--;
+		zoomLevel--;
 
 	}
 
@@ -244,6 +235,128 @@ else {
 	// Restore application preferences
 	app.preferences.rulerUnits = startRulerUnits;
 
+}
+
+
+// **** UI Windows and Logic
+
+var windowLoading = new Window(
+	'palette {' +
+	'	text: "Google Maps Tile Cutter (Cutting Tiles, Please Wait)",' +
+	'	alignChildren: "center",' +
+	'	bounds: [0, 0, 300, 50]' +
+//	'	bar: Progressbar{ bounds: [20,20,280,31] , value: 0, maxvalue: 100 }' +
+	'	txtStatus: StaticText{ bounds: [20, 18, 280, 31] , text: "Preprocessing Image ...", alignment: "center" }' +
+	'}',
+	'Google Maps Tile Cutter (Cutting Tiles, Please Wait)',
+	undefined,
+	{
+		closeButton: false
+	}
+);
+
+var windowMain = new Window(
+	'dialog {' +
+	'	text:"Google Maps Tile Cutter", alignChildren: "fill",' +
+	'	pnlExportDir: Panel {' +
+	'		text: "EXPORT PATH",' +
+	'		orientation: "column", alignChildren: ["left", "top"],' +
+	'		grpExport: Group {' +
+	'			orientation: "row", alignment: "left",' +
+	'			txtExportPath: EditText { text: "", characters: 50, enabled: false },' +
+	'			btnExportPath: Button { text:"Choose" }' +
+	'		}' +
+	'	}' +
+	'	pnlExportOptions: Panel { orientation: "column", alignChildren: ["left", "top"],' +
+	'		text: "CUTTING OPTIONS",' +
+	'		grpFiletype: Group { orientation: "row", alignment: "left",' +
+	'			lblExportAs: StaticText { text: "Export as" },' +
+	'			optJPEG: RadioButton { text: "JPEG", value: true },' +
+	'			optPNG: RadioButton { text: "PNG" },' +
+	'			optGIF: RadioButton { text: "GIF" }' +
+	'		}' +
+	'		grpSizeColor: Group { orientation: "row", alignment: "left",' +
+	'			lblSize: StaticText { text: "Tile Size:" },' +
+	'			txtSize: EditText { text: "256", characters: 4, enabled: true },' +
+	'			lblBg: StaticText { text: "Background Color:" },' +
+	'			txtBgColor: EditText { text:"' + app.backgroundColor.rgb.hexValue + '", characters: 6, enabled: true }' +
+	'		}' +
+	'		grpExportBlanks: Group { orientation: "row", alignment: "left",' +
+	'			cbDontExport: Checkbox { text: "Don\'t export transparent tiles", value: true},' +
+	'		}' +
+	'	}' +
+	'	grpButtons: Group {' +
+	'		orientation: "row", alignment: "right",' +
+	'		txtHowto: StaticText { text: "Please select an export directory to begin" },' +
+	'		btnMakeTiles: Button { text: "Make Tiles", enabled: false, properties: { name: "ok" } }' +
+	'	}' +
+	'}',
+	'Google Maps Tile Cutter (Press ESC to cancel)',
+	undefined,
+	{
+		closeButton: false
+	}
+);
+
+// When clicking the 'Choose' button, show a select dialog and store the value into the textfield
+windowMain.pnlExportDir.grpExport.btnExportPath.onClick = function() {
+	folder = Folder.selectDialog();
+	if (folder !== null) {
+		windowMain.pnlExportDir.grpExport.txtExportPath.text = folder.fsName;
+		windowMain.grpButtons.txtHowto.text = 'Click "Make Tiles" to generate your tiles';
+		windowMain.grpButtons.btnMakeTiles.enabled = true;
+		windowMain.grpButtons.btnMakeTiles.active = true;
+	}
+};
+
+// Focus the 'Choose' button on load
+windowMain.onShow = function() {
+	windowMain.pnlExportDir.grpExport.btnExportPath.active = true;
+};
+
+
+// Generate the tiles, when clicking the "Make Tiles" button
+windowMain.grpButtons.btnMakeTiles.onClick = function() {
+
+	// Extract the options
+	var options = {
+		targetPath: windowMain.pnlExportDir.grpExport.txtExportPath.text,
+		tileSize: parseInt(windowMain.pnlExportOptions.grpSizeColor.txtSize.text, 10),
+		saveTransparentTiles: !windowMain.pnlExportOptions.grpExportBlanks.cbDontExport.value,
+		saveJPEG: windowMain.pnlExportOptions.grpFiletype.optJPEG.value,
+		savePNG: windowMain.pnlExportOptions.grpFiletype.optPNG.value,
+		saveGIF: windowMain.pnlExportOptions.grpFiletype.optGIF.value,
+		bgColor: windowMain.pnlExportOptions.grpSizeColor.txtBgColor.text
+	};
+
+	// Hide main window and show the loading window
+	windowMain.hide();
+	windowLoading.show();
+	windowLoading.center(); // Hmmz, this still is a bit off ...
+
+	// Cut the tiles
+	cutTiles(options, function(curTile, totalTiles) {
+		windowLoading.txtStatus.text = 'Cutting Tile ' + curTile + '/' + totalTiles + ' (' + Math.floor(curTile / totalTiles * 100) + '%)';
+		// windowLoading.bar.value = parseInt(windowLoading.bar.value, 10) + 1;
+	});
+
+	// Close the windowMain
+	windowLoading.close();
+
+};
+
+// **** MAIN CORE: Show the main window if a document is open
+
+// No active document: stop here
+if (app.documents.length === 0) {
+	alert("Please open a file", "Google Maps Tile Cutter Error", true);
+	app.beep();
+}
+
+// Active document: show windowMain
+else {
+	windowMain.center();
+	windowMain.show();
 }
 
 // EOF
